f1cf9891e448862c359de6fc42634273bc75d7bf
daisy <paipaicui@163.com>
2020-07-13 18:43:02 +0800

    new

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -2,10 +2,10 @@
   <div>
     <van-nav-bar title="CRM" left-arrow>
       <template #left>
-
+        <span></span>
       </template>
       <template #right>
-
+        <span></span>
       </template>
     </van-nav-bar>
   </div>
