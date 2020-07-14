955bc5b69ec07d2ee25114e5ff244f1f42b02ed6
daisy <paipaicui@163.com>
2020-07-14 18:02:05 +0800

    new package

--- src/pages/home/home1.vue
+++ src/pages/home/home1.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>home1</div>
+
+  <ul>
+    <li class="cell"></li>
+  </ul>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style lang="less" scoped>
+.cell {
+  padding: 0.16rem 0.16rem 0.16rem 0.44rem;
+  border-bottom: 1px solid #e5e5e5;
+}
 </style>
